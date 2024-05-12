b8b77f4dc2a839f9da9f91a239b4f41300b63dbc
gabriela <gabriellasilver.05@gmail.com>
2024-05-12 14:59:44 -0300

    projeto finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo:");
+        double saldo;
+        try {
+            saldo = Double.parseDouble(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Por favor, insira um valor numérico válido para o saldo.");
+            return;
+        }
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+        agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
